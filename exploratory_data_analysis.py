0c7fcb93c1b620c90c288580bb586c26b09f1f8d
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2019-03-15 20:16:11 +0530

    added some code to explore using notebook

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -10,6 +10,8 @@ import io
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.core.display import Image, display
+import nbformat as nbf
 
 
 def parse_cmdline():
@@ -17,6 +19,9 @@ def parse_cmdline():
     parser.add_argument('inputfile',
                         type=str,                    
                         help='Inputfile')
+    parser.add_argument('target_column',
+                        type=str,                    
+                        help='target_column')
     parser.add_argument('configfile',
                          type=str,                    
                          help='configfile')
@@ -109,31 +114,108 @@ def generate_plots_df(type_, df, name, plots_location):
     fig = plot_.get_figure()
     fig.savefig('{}/{}_{}.png'.format(plots_location, name, type_))
 
+def get_in_between(df, target_column, a, b):
+    tc = target_column
+    eda_logger.info("In between {} and {}".format(a, b))
+    return df[(df[tc] > a) & (df[tc] <= b)][target_column]
+    
 
-def simple_analysis(inputfile, configfile):
+def create_notebook(plots_location, notebook):
+    cells = notebook['cells']
+
+    cells.append(nbf.v4.new_code_cell("from IPython.core.display import Image, display"))
+    for filename in os.listdir(plots_location):
+        if 'png' in filename:
+            full_filename = os.path.join(plots_location, filename)
+            cells.append(nbf.v4.new_markdown_cell(full_filename))
+            code_ = "display(Image(filename='{}'))".format(full_filename)
+            cells.append(nbf.v4.new_code_cell(code_))
+
+def simple_analysis(inputfile, configfile, target_column):
     df = pd.read_csv(inputfile)
     cnf = config_parser.get_configuration(configfile)
     plots_location = cnf.get_directory('exploratory_data_analysis')
+    src_location = cnf.get_source_location()
+    notebook = nbf.v4.new_notebook()
+    cells = notebook['cells']
+    src_filename = 'exploratory_data_analysis.py'
+    tempfilename = '{}/tmp_{}'.format(src_location, src_filename)
+    os.system('head -n -11 {} > {}'.format(src_filename, tempfilename))
+    cells.append(nbf.v4.new_code_cell("%load {}".format(tempfilename)))
+    notebook_start = """from collections import Counter
+import logger
+import config_parser
+import argparse
+import utils
+import pandas as pd
+import numpy as np
+import io
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.core.display import Image, display
+import nbformat as nbf
+df = pd.read_csv('{inputfile}')
+""".format(inputfile=inputfile)
+    cells.append(nbf.v4.new_code_cell(notebook_start))
 
     eda_logger.info(df.shape)
+    cells.append(nbf.v4.new_code_cell("df.shape"))
     eda_logger.info(df.head(5))
+    cells.append(nbf.v4.new_code_cell("df.head(5)"))
     eda_logger.info(str(df.describe()))
+    cells.append(nbf.v4.new_code_cell("df.describe()"))
     eda_logger.info(get_df_info(df))
+    cells.append(nbf.v4.new_code_cell("df.info()"))
+    numerical_columns = utils.get_numerical_columns(df)
+    code_ = """numerical_columns = utils.get_numerical_columns(df)
+numerical_columns"""
+    str_ = "Numerical columns are {}".format(numerical_columns)
+    eda_logger.info(str_)
+    cells.append(nbf.v4.new_markdown_cell(str_))
+    cells.append(nbf.v4.new_code_cell(code_))
+    outliers_info = {'column': [], 'outliers_rate': []}
     for column in df.columns:
+        code_ = 'column="{}"'.format(column)
+        cells.append(nbf.v4.new_code_cell(code_))
         try:
-            eda_logger.info("Here are the outliers in column {}".format(column))
-            outliers = find_outliers(df[column])
-            eda_logger.info(outliers)
-            fraction_of_outliers = len(outliers) / len(df[column])
-            eda_logger.info("fraction of outliers is {}".format(fraction_of_outliers))
+            if column in numerical_columns:
+                str_ = "Here are the outliers in column {}".format(column)
+                eda_logger.info(str_)
+                cells.append(nbf.v4.new_markdown_cell(str_))
+                outliers = find_outliers(df[column])
+                code_ = "outliers = find_outliers(df[column])"
+                eda_logger.info(outliers)
+                cells.append(nbf.v4.new_code_cell(code_))
+                fraction_of_outliers = len(outliers) / len(df[column])
+                code_ = "fraction_of_outliers = len(outliers) / len(df[column])"
+                cells.append(nbf.v4.new_code_cell(code_))
+                str_ = "fraction of outliers is {}".format(fraction_of_outliers)
+                outliers_info['column'].append(column)
+                outliers_info['outliers_rate'].append(fraction_of_outliers)
+                eda_logger.info(str_)
+                cells.append(nbf.v4.new_markdown_cell(str_))
         except:
             eda_logger.error('Exception happened while processing column {} outliers'.format(column))
         try:
             unique_values = df[column].unique()
             num_unique_values = len(unique_values)
-            eda_logger.info("number of unique values in column {} is {}".format(column, num_unique_values))
+            code_ = """unique_values = df[column].unique()
+num_unique_values = len(unique_values)"""
+            str_ = "number of unique values in column {} is {}".format(column, num_unique_values)
+            eda_logger.info(str_)
+            cells.append(nbf.v4.new_code_cell(code_))
+            cells.append(nbf.v4.new_markdown_cell(str_))
+
+
             value_counts = df[column].value_counts()
-            eda_logger.info("Top 10 value counts for the column {} are \n{}".format(column, value_counts.head(10)))
+            code_ = """value_counts = df[column].value_counts()
+value_counts"""
+            str_ = "Top 10 value counts for the column {} are \n{}".format(column, value_counts.head(10))
+            eda_logger.info(str_)
+            cells.append(nbf.v4.new_code_cell(code_))
+            cells.append(nbf.v4.new_markdown_cell(str_))
+
 
             for plot_type in ['boxplot', 'violinplot']:
                 generate_plots(plot_type, df[column], column, plots_location)
@@ -141,15 +223,58 @@ def simple_analysis(inputfile, configfile):
             eda_logger.error('Exception happened while processing column {}'.format(column))
 
     try:
-        generate_plots_df('heatmap', df, 'df', plots_location)
-        generate_plots_df('correlation', df, 'df', plots_location)
-    except:
+        numerical_df = df[numerical_columns]
+        df_corr = numerical_df.corr()
+        code_ = """numerical_df = df[numerical_columns]
+df_corr = numerical_df.corr()
+df_corr"""
+        cells.append(nbf.v4.new_code_cell(code_))
+        eda_logger.info(df_corr[target_column].sort_values())
+        code_ = """target_column='{}'
+df_corr[target_column].sort_values(ascending=False)""".format(target_column)
+        cells.append(nbf.v4.new_code_cell(code_))
+        eda_logger.info(get_in_between(df_corr, target_column, .5, 1))
+        eda_logger.info(get_in_between(df_corr, target_column, .0, .5))
+        eda_logger.info(get_in_between(df_corr, target_column, -.5, 0))
+        eda_logger.info(get_in_between(df_corr, target_column, -1, -.5))
+        generate_plots_df('heatmap', numerical_df, 'numerical', plots_location)
+        generate_plots_df('correlation', numerical_df, 'numerical', plots_location)
+
+        code_ = """null_rate_numerical_df = utils.get_null_rate(numerical_df)
+null_rate_numerical_df"""
+        null_rate = utils.get_null_rate(numerical_df)
+        eda_logger.info(null_rate)
+        cells.append(nbf.v4.new_code_cell(code_))
+
+
+        code_ = """null_rate_df = utils.get_null_rate(df)
+null_rate_df"""
+        null_rate = utils.get_null_rate(df)
+        eda_logger.info(null_rate)
+        cells.append(nbf.v4.new_code_cell(code_))
+
+        code_ = """outliers_info={}
+outliers_df = pd.DataFrame(outliers_info).sort_values(['outliers_rate'], ascending=False)
+outliers_df""".format(outliers_info)
+        outliers_df = pd.DataFrame(outliers_info).sort_values(['outliers_rate'], ascending=False)
+        eda_logger.info(outliers_df)
+        cells.append(nbf.v4.new_code_cell(code_))
+    except Exception as e:
         eda_logger.error('Exception happened while processing heatmap')
+        print(e)
         
 
+    # create_notebook(plots_location, notebook)
+    nbf.write(notebook, os.path.join(src_location, 'eda.ipynb'))
+        
 
 def main(args):
-    simple_analysis(args.inputfile, args.configfile)
+    simple_analysis(args.inputfile, args.configfile, args.target_column)
+
+
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -22,3 +22,64 @@ def load(filename):
     with open(filename, 'rb') as f:
         obj = pickle.load(f)
     return obj
+
+def get_type(s):
+    """ Return the type of series item
+    """
+    return s.dtype
+
+def is_numerical(s):
+    """ Returns whether a series is of type numerical"""
+    x = get_type(s)
+    if x == 'float64' or x == 'int64':
+        return True
+    return False
+
+
+def get_numerical_columns(df):
+    """ Return the numerical columns of data frame
+    """
+    columns = []
+    for column in df.columns:
+        if is_numerical(df[column]):
+            columns.append(column)
+            
+    return columns
+
+def get_columns_with_null(df):
+    """ Return columns of data frame with null values
+    """
+    columns = []
+    for column in df.columns:
+        if (df[column].isnull().any()):
+            columns.append(column)
+            
+    return columns
+
+
+def get_numerical_columns_with_null(df):
+    """ Return columns of data frame with null values
+    """
+    columns = []
+    for column in df.columns:
+        if is_numerical(df[column]):
+            if (df[column].isnull().any()):
+                columns.append(column)
+            
+    return columns
+            
+
+def get_null_rate(df):
+    """ Given the null rate of each numerical columns of df
+    """
+    columns = dict()
+    for column in df.columns:
+        if (df[column].isnull().any()):
+            columns[column] = df[column].isnull().sum() / len(df)
+
+    columns_ = {'column': [], 'null_rate': []}
+    for column, value in columns.items():
+        columns_['column'].append(column)
+        columns_['null_rate'].append(value)
+    return pd.DataFrame(columns_).sort_values(['null_rate'], ascending=False)
+    
